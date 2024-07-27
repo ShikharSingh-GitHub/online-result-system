db2d367b822cec5a71d10a18bb1786d9e05bc985
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-07-27 12:09:41 +0530

    created and fetched APIs

--- orsfe/src/App.js
+++ orsfe/src/App.js
@@ -1,37 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
 import UserForm from './components/UserForm';
 import UserList from './components/UserList';
 
-function App() {
+const App = () => {
+
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    fetch('http://localhost:5000/api/user/searchuser')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        setUsers(data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <Router>
-      <div>
+      <div className="container">
+        <h1>User Management</h1>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home ||</Link>
-            </li>
-            <li>
-              <Link to="/UserList">UserList ||</Link>
-            </li>
-            <li>
-              <Link to="/UserForm">UserForm ||</Link>
-            </li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/adduser">Add User</Link></li>
+            <li><Link to="/UserList">UserList</Link></li>
           </ul>
         </nav>
-
         <Routes>
-        <Route path="/" element={<h1>welcome to job jungle</h1>} />
-          <Route path="/UserList" element={<UserList />} />
-          <Route path="/UserForm" element={<UserForm/>} />
+          <Route path="/" element={<h2>Welcome to the User Management System</h2>} />
+          <Route path="/adduser" element={<UserForm fetchUsers={fetchUsers} />} />
+          <Route path="/UserList" element={<UserList users={users} />} />
         </Routes>
       </div>
     </Router>
   );
-}
-
+};
 
 export default App;
-
--- orsfe/src/components/UserList.js
+++ orsfe/src/components/UserList.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-function UserList() {
+const UserList = ({ users }) => {
   return (
-    <div className="UserList">
-      <h1>User List</h1>
+    <div>
+      <h2 align="center">User List</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Login ID</th>
+            <th>DOB</th>
+            <th>Gender</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.loginId}</td>
+              <td>{new Date(user.dob).toLocaleDateString()}</td>
+              <td>{user.gender}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default UserList;
