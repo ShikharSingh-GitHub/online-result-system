d18b980658c149cb480cfe425c4be84d05e5ee8c
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-08-07 18:52:45 +0530

    Dockerised

--- expressjs/db.js
+++ expressjs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mernproject');
+mongoose.connect('mongodb://mongo:27017/mernproject');
 
 mongoose.connection.on('connected', () => {
     console.log('Connected to MongoDB');
